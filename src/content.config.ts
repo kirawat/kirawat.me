5c8badf6b88b1ae5cafdbc73b0cc24dd5d46a266
Kirawat <kirawats@gmail.com>
2025-05-28 08:56:39 +0700

    wip: Site Styling

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,12 +2,27 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const basePostSchema = z.object({
+  /** The title of the current page. Required. */
   title: z.string(),
+
+  /**
+   * A short description of the current page's content.
+   * Optional, but recommended.
+   * 
+   * A good description is 150-160 characters long and outlines the key content
+   * of the page in a clear and engaging way.
+   */
   description: z.string().optional(),
   publishDate: z.coerce.date(),
   modifiedDate: z.coerce.date().optional(),
   tags: z.array(z.string()).optional(),
-  draft: z.boolean().optional(),
+
+  /**
+   * Indicates that this page is a draft and will not be included
+   * in production builds. Note that the page will still be available
+   * when running Astro in development mode.
+   */
+  draft: z.boolean().default(false),
 });
 
 const evergreenPostSchema = z.object({
